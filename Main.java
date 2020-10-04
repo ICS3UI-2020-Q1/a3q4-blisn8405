88adc5b486ed4ebf81e7186c18bf10571db47f7b
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:49:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,45 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+ // create a scanner for user input
+Scanner input = new Scanner(System.in); 
+    // get the user choice between 1 and 5
+ System.out.println("Please enter a positive integer");
+ int choice = input.nextInt();
+ 
+   switch (choice){
+ case 1:
+  System.out.println("one");
+   break;
+   case 2:
+  System.out.println("two");
+   break;
+   case 3:
+  System.out.println("three");
+   break;
+   case 4:
+  System.out.println("four");
+   break;
+   case 5:
+  System.out.println("five");
+   break;
+   case 6:
+  System.out.println("six");
+   break;
+   case 7:
+  System.out.println("seven");
+   break;
+  case 8:
+  System.out.println("eight");
+   break;
+   case 9:
+  System.out.println("nine");
+   break;
+   case 10:
+  System.out.println("ten");
+   break;
+   default:
+   System.out.println("Not in the range");
   }
+}
 }
